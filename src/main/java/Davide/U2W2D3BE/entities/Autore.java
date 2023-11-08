8c235d65a5aa0ccd0f31d545c532a3dc5317e1a0
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-08 13:51:23 +0100

    add dependency, Autore into Entity in db

--- src/main/java/Davide/U2W2D3BE/entities/Autore.java
+++ src/main/java/Davide/U2W2D3BE/entities/Autore.java
@@ -1,21 +1,31 @@
 package Davide.U2W2D3BE.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Autore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String cognome;
     private String email;
     private LocalDate dataDiNascita;
     private String avatar;
+    @CreationTimestamp
+    private LocalDate createdAt;
 
     @Override
     public boolean equals(Object o) {
---
+++ src/main/java/Davide/U2W2D3BE/repositories/AutoreRepository.java
@@ -0,0 +1,7 @@
+package Davide.U2W2D3BE.repositories;
+
+import Davide.U2W2D3BE.entities.Autore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoreRepository extends JpaRepository<Autore, Long > {
+}
