c3f76bdee96d6f57c7f78c72c9fc05d63ce8cc0a
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-08 14:29:25 +0100

    update port where running postgres

--- src/main/java/Davide/U2W2D3BE/Services/AutoreService.java
+++ src/main/java/Davide/U2W2D3BE/Services/AutoreService.java
@@ -2,6 +2,8 @@ package Davide.U2W2D3BE.Services;
 
 import Davide.U2W2D3BE.entities.Autore;
 import Davide.U2W2D3BE.exceptions.NotFoundException;
+import Davide.U2W2D3BE.repositories.AutoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,43 +12,31 @@ import java.util.Random;
 
 @Service
 public class AutoreService {
-    private List<Autore> autori= new ArrayList<>();
+    @Autowired
+    private AutoreRepository autoreRepo;
 
     public long save(Autore body){
-        Random random = new Random();
-        body.setId(random.nextInt(1001, 2000));
         body.setAvatar("https://ui-avatars.com/api/?name=" + body.getNome() + "+" + body.getCognome());
-        this.autori.add(body);
+        autoreRepo.save(body);
         return body.getId();
     }
 
     public List<Autore> getAll(){
-        return this.autori;
+        return autoreRepo.findAll();
     }
     public Autore getSingleAutore(long id){
-        Autore found = null;
-        for (Autore autore: this.autori) {
-            if (autore.getId() == id){
-                found = autore;
-            }
-        }
-        if (found != null) {
-            return found;
-        }else {
-            throw new NotFoundException(id);
-        }
+        return autoreRepo.findById(id).orElseThrow(()-> new NotFoundException(id) );
     }
     public Autore updateAnAutore(long id, Autore body){
-        Autore updatedAutore= this.getSingleAutore(id);
-        updatedAutore.setNome(body.getNome());
-        updatedAutore.setCognome(body.getCognome());
-        updatedAutore.setEmail(body.getEmail());
-        updatedAutore.setDataDiNascita(body.getDataDiNascita());
-        updatedAutore.setAvatar("https://ui-avatars.com/api/?name=" + body.getNome() + "+" + body.getCognome());
-        return updatedAutore;
+        Autore found = autoreRepo.findById(id).orElseThrow(() -> new NotFoundException(id));
+        found.setNome(body.getNome());
+        found.setCognome(body.getCognome());
+        found.setEmail(body.getEmail());
+        found.setDataDiNascita(body.getDataDiNascita());
+        found.setAvatar("https://ui-avatars.com/api/?name=" + body.getNome() + "+" + body.getCognome());
+        return found;
     }
     public void deleteAnAutore(long id){
-        Autore toRemove = this.getSingleAutore(id);
-        this.autori.remove(toRemove);
+        autoreRepo.deleteById(id);
     }
 }
